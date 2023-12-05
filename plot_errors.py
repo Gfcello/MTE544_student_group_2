4af51b9b8f893d49facf474459283c0af9953f1a
Maximo van der Raadt <maximovan@me.com>
2023-12-05 18:49:27 -0500

    ccomment for the plotting fiole

--- plot_errors.py
+++ plot_errors.py
@@ -3,6 +3,7 @@ from utilities import FileReader
 
 def plot_errors(files):
 
+    # Plot only one robotPose.csv file
     if (len(files) == 1):
         filename = files[0]
         headers, values=FileReader(filename).read_file()
@@ -14,18 +15,21 @@ def plot_errors(files):
         for val in values:
             time_list.append(val[-1] - first_stamp)
 
-        # Plot pose of robot on map
+        # Isolate x and y poses of robot from data
         x = [lin[0] for lin in values]
         y = [lin[1] for lin in values]
 
         plt.plot(x, y, label = "Trajectory")
 
+        # Set label for plot title based on heuristic used
         if "man" in filename:
             heuristic = "Manhattan"
         else:
             heuristic = "Euclidian"
 
+    # Plot two robotPose.csv file (both paths for one heuristic)
     elif (len(files) == 2):
+        # Everything is the same as the 1 file case, just doubled
         file1, file2 = files
         headers1, values1=FileReader(file1).read_file()
         headers2, values2=FileReader(file2).read_file()
@@ -42,31 +46,33 @@ def plot_errors(files):
         for val in values2:
             time_list2.append(val[-1] - first_stamp2)
 
+        # Plot image of the map in background
         im = plt.imread('room.pgm')
         fig, ax = plt.subplots()
+        # Image dimensions taken by measuring the map in RVIZ and using the offset
+        # from the yaml file
         im = ax.imshow(im, extent = [-7.699941, 5.019859, -2.500001 , 5.590559])
 
-        # Plot pose of robot on map
+        # Isolate x and y poses for each trajectory
         x1 = [lin[0] for lin in values1]
         y1 = [lin[1] for lin in values1]
 
         x2 = [lin[0] for lin in values2]
         y2 = [lin[1] for lin in values2]
 
+        # Get heuristic label for graph title, assume both files are the same heuristic
         if "man" in file1:
             heuristic = "Manhattan"
         else:
             heuristic = "Euclidian"
 
+        # Add the grid, trajectories, start and goal points to plot
         ax.grid(zorder=-1)
         ax.plot(x1, y1, label = "Goal 1 Trajectory", color="red", zorder=0)
         ax.plot(x2, y2, label = "Goal 2 Trajectory", color="blue", zorder=0)
         ax.scatter(x1[0], y1[0], color="red", label = "Start Point", zorder=1)
         ax.scatter(x1[-1], y1[-1], color="purple", label = "Goal 1", zorder=1)
         ax.scatter(x2[-1], y2[-1], color="blue", label="Goal 2", zorder=1)
-    # else:
-    #     for i in range(0, len(headers) - 1):
-    #         plt.plot(time_list, [lin[i] for lin in values], label= headers[i]+ " linear")
 
     plt.legend()
     plt.title("X vs Y Trajectory Using " + heuristic + " Distance")
